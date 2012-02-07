e621f09234acec9ecec1169f0fc1f051bb1d2d52
Titus Soporan <titus@tsoporan.com>
2012-02-06 19:36:48 -0500

    initial support for google plus

--- hyve.js
+++ hyve.js
@@ -986,3 +986,32 @@ hyve.feeds['foursquare'] = {
         }
     }
 };
+
+hyve.feeds['plus'] = {
+    interval : 5000,
+    methods : ['search'],
+    api_key : '', 
+    feed_url : 'https://www.googleapis.com/plus/v1/activities?query={{query}}&language=en&orderBy=best&maxResults=20&pp=1&key={{api_key}}{{&#callback=#callback}}',
+    parse : function(data, query, callback) {
+        if (!this.items_seen){
+            this.items_seen = {};
+        }
+        if (data.items){
+            data.items.forEach(function(item){
+                if (!this.items_seen[item.id]){
+                    this.items_seen[item.id] = true;
+                    
+					hyve.process({
+                        'service' : 'plus',
+                        'type' : 'text',
+                        'query' : query,
+                        'id' : item.id,
+                        'date' : item.published,
+                        'text' : item.title,
+                        'source' : item.url,
+                    },callback);
+                }
+            }, this);
+        }
+    }
+};
