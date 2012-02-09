70a41085ca7c490daaed21940b7ea6a7bd4b13e4
lrvick <lance@lrvick.net>
2012-02-09 04:41:57 -0500

    fix callback in feed_url & add initial link support

--- hyve.js
+++ hyve.js
@@ -990,8 +990,8 @@ hyve.feeds['foursquare'] = {
 hyve.feeds['plus'] = {
     interval : 5000,
     methods : ['search'],
-    api_key : '', 
-    feed_url : 'https://www.googleapis.com/plus/v1/activities?query={{query}}&language=en&orderBy=best&maxResults=20&pp=1&key={{api_key}}{{&#callback=#callback}}',
+    api_key : '',
+    feed_url : 'https://www.googleapis.com/plus/v1/activities?query={{query}}&language=en&orderBy=best&maxResults=20&pp=1&key={{api_key}}{{#&callback=#callback}}',
     parse : function(data, query, callback) {
         if (!this.items_seen){
             this.items_seen = {};
@@ -1000,15 +1000,20 @@ hyve.feeds['plus'] = {
             data.items.forEach(function(item){
                 if (!this.items_seen[item.id]){
                     this.items_seen[item.id] = true;
-                    
+                    item.type = 'text';
+                    if (!item.title){
+                        item.type = 'link';
+                        item.url = item.object.attachments[0].url;
+                        item.title = item.object.attachments[0].displayName;
+                    }
                     hyve.process({
                         'service' : 'plus',
-                        'type' : 'text',
+                        'type' : item.type,
                         'query' : query,
                         'id' : item.id,
                         'date' : item.published,
                         'text' : item.title,
-                        'source' : item.url,
+                        'source' : item.url
                     },callback);
                 }
             }, this);
