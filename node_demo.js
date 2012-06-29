a9de7b12615c06bc7865ee1751056b77a28e07c4
lrvick <lance@lrvick.net>
2012-06-29 05:56:54 -0400

    first (non working) attempt at plugin files

--- node_demo.js
+++ node_demo.js
@@ -2,6 +2,8 @@
 
 var hyve = require('./hyve.js')
 
+require('./plugins/twitter.js')
+
 hyve.queue_enable = false
 hyve.recall_enable = false
 
@@ -11,12 +13,6 @@ console.log('Starting Stream')
 // hyve.friends.stream(callback, [services])
 // hyve.search.stream(query, callback, [services])
 
-//hyve.search.stream('android', function(data){
-//    console.log(data.service +' : '+ data.text)
-//}, ['twitter',])
-
-hyve.feeds.youtube.auth_user = '' // set for friends
-
-hyve.friends.stream(function(data) {
-  console.log(data.service + ' : ' + data.text)
-},['flickr',])
+hyve.search.stream('android', function(data){
+    console.log(data.service +' : '+ data.text)
+}, ['twitter'])
