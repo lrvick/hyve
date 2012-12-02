1608558b71b3972a563f6bbe2b19a200299b115f
Nicholas Bardy <nicholasbardy@yahoo.com>
2012-12-01 21:54:35 -0500

    Instagram bug fix and added support for search

--- src/hyve.instagram.js
+++ src/hyve.instagram.js
@@ -1,14 +1,14 @@
 (function(root) {
-
     var hyve = (typeof require == 'function' && !(typeof define == 'function' && define.amd)) ? require('../src/hyve.core.js') : root.hyve
 
     hyve.feeds['instagram'] = {
-        methods : ['friends'],
+        methods : ['friends', 'search'],
         interval : 3000,
         interval_friends : 10000,
         access_token : '',
         feed_urls : {
-            friends: 'https://api.instagram.com/v1/users/self/feed?limit=25&type=post{{ access_token }}{{ since }}{{#&callback=#callback}}'
+            friends: 'https://api.instagram.com/v1/users/self/feed?limit=25&type=post{{ access_token }}{{ since }}{{#&callback=#callback}}',
+            search: 'https://api.instagram.com/v1/tags/{{query}}/media/recent?limit=25&type=post{{ access_token }}{{ since }}{{#&callback=#callback}}'
         },
         format_url : function(query){
             var since_arg = ''
@@ -22,40 +22,43 @@
             }
         },
         parsers : {
-            friends: function(data, query, callback){
-                if (data && data.data && data.data.length > 0){
-                    hyve.feeds.instagram.since = data.data[0].id
-
-                    data.data.forEach(function(item){
-
-                        var text = undefined;
-
-                        if (item.caption) text = item.caption.text
-
-                        hyve.process({
-                            'service' : 'instagram',
-                            'type' : 'image',
-                            'query' : query,
-                            'user' : {
-                                'id' : item.user.id,
-                                'name' : item.user.username,
-                                'real_name' : item.user.full_name,
-                                'avatar' : item.user.profile_picture
-                            },
-                            'id' : item.id,
-                            'date' : item.created_time,
-                            'text' : text,
-                            'thumbnail' : item.images.standard_resolution.url,
-                            'comments' : item.comments.count,
-                            'source' : item.link,
-                            'likes' : item.likes.count,
-                            'weight' : item.likes.count + item.comments.count
-                        },callback)
-
-                    },this)
-                }
-            }
+            search: parseData,
+            friends: parseData
         }
     }
+
+function parseData(data, query, callback){
+    if (data && data.data && data.data.length > 0){
+        hyve.feeds.instagram.since = data.pagination.next_min_id
+
+        data.data.forEach(function(item){
+
+            var text = undefined;
+
+            if (item.caption) text = item.caption.text
+
+            hyve.process({
+                'service' : 'instagram',
+                'type' : 'image',
+                'query' : query,
+                'user' : {
+                    'id' : item.user.id,
+                    'name' : item.user.username,
+                    'real_name' : item.user.full_name,
+                    'avatar' : item.user.profile_picture
+                },
+                'id' : item.id,
+                'date' : item.created_time,
+                'text' : text,
+                'thumbnail' : item.images.standard_resolution.url,
+                'comments' : item.comments.count,
+                'source' : item.link,
+                'likes' : item.likes.count,
+                'weight' : item.likes.count + item.comments.count
+            },callback)
+
+        },this)
+    }
+}
 
 })(this)
